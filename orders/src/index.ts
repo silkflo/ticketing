cd8157b352de97a49b908103c95e2475cecea698
silkflo <florian.assous@hotmail.fr>
2022-06-15 20:49:20 +0700

    add startup message to order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 
 const start = async () => {
   //by the error check , it tell typescrit that the env is not undefined but string
+  console.log('Starting...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
